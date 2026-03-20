533c7025734a6503faf96c6d09510dad3079d376
tpoeira123 <tpoeira2005@gmail.com>
2026-03-20 15:07:24 +0000

    task6 not done

--- src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/LoginResource.java
+++ src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/LoginResource.java
@@ -376,6 +376,109 @@ public class LoginResource {
 	}
 
 
+	// task6
+	@POST
+	@Path("/v2")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doLoginV2(LoginData data,
+							  @Context HttpServletRequest request,
+							  @Context HttpHeaders headers) {
+		LOG.fine(LOG_MESSAGE_LOGIN_ATTEMP + data.username);
+
+		Key userKey = userKeyFactory.newKey(data.username);
+		Key ctrsKey = datastore.newKeyFactory()
+				.addAncestors(PathElement.of("User", data.username))
+				.setKind("UserStats")
+				.newKey("counters");
+		// Generate automatically a key
+		Key logKey = datastore.allocateId(
+				datastore.newKeyFactory()
+						.addAncestors(PathElement.of("User", data.username))
+						.setKind("UserLog").newKey());
+
+		Transaction txn = datastore.newTransaction();
+		try {
+			Entity user = txn.get(userKey);
+			if (user == null) {
+				// Username does not exist
+				LOG.warning(LOG_MESSAGE_LOGIN_ATTEMP + data.username);
+				return Response.status(Status.FORBIDDEN)
+						.entity(MESSAGE_INVALID_CREDENTIALS)
+						.build();
+			}
+
+			// We get the user stats from the storage
+			Entity stats = txn.get(ctrsKey);
+			if (stats == null) {
+				stats = Entity.newBuilder(ctrsKey)
+						.set("user_stats_logins", 0L)
+						.set("user_stats_failed", 0L)
+						.set("user_first_login", Timestamp.now())
+						.set("user_last_login", Timestamp.now())
+						.build();
+			}
+
+			String hashedPWD = (String) user.getString(USER_PWD);
+			if (hashedPWD.equals(DigestUtils.sha512Hex(data.password))) {
+				// Login successful
+				// Construct the logs
+				String cityLatLong = headers.getHeaderString("X-AppEngine-CityLatLong");
+				Entity log = Entity.newBuilder(logKey)
+						.set("user_login_ip", request.getRemoteAddr())
+						.set("user_login_host", request.getRemoteHost())
+						.set("user_login_latlon", cityLatLong != null
+								? StringValue.newBuilder(cityLatLong).setExcludeFromIndexes(true).build()
+								: StringValue.newBuilder("").setExcludeFromIndexes(true).build())
+						.set("user_login_city", headers.getHeaderString("X-AppEngine-City"))
+						.set("user_login_country", headers.getHeaderString("X-AppEngine-Country"))
+						.set("user_login_time", Timestamp.now())
+						.build();
+
+				// Get the user statistics and updates it
+				// Copying information every time a user logins may not be a good solution
+				// (why?)
+				Entity ustats = Entity.newBuilder(ctrsKey)
+						.set("user_stats_logins", stats.getLong("user_stats_logins") + 1)
+						.set("user_stats_failed", 0L)
+						.set("user_first_login", stats.getTimestamp("user_first_login"))
+						.set("user_last_login", Timestamp.now())
+						.build();
+
+				// Batch operation
+				txn.put(log, ustats);
+				txn.commit();
+
+				// Return token
+				AuthToken token = new AuthToken(data.username);
+				LOG.info(LOG_MESSAGE_LOGIN_SUCCESSFUL + data.username);
+				return Response.ok(g.toJson(token)).build();
+			} else {
+				// Incorrect password
+				// Copying here is even worse. Propose a better solution!
+				Entity ustats = Entity.newBuilder(ctrsKey)
+						.set("user_stats_logins", stats.getLong("user_stats_logins"))
+						.set("user_stats_failed", stats.getLong("user_stats_failed") + 1L)
+						.set("user_first_login", stats.getTimestamp("user_first_login"))
+						.set("user_last_login", stats.getTimestamp("user_last_login"))
+						.set("user_last_attempt", Timestamp.now())
+						.build();
+
+				txn.put(ustats);
+				txn.commit();
+				LOG.warning(LOG_MESSAGE_WRONG_PASSWORD + data.username);
+				return Response.status(Status.FORBIDDEN).entity(MESSAGE_INVALID_CREDENTIALS).build();
+			}
+		} catch (Exception e) {
+			txn.rollback();
+			LOG.severe(e.getMessage());
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		} finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
+	}
 
 
 
